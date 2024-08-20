e2f86cc5631e541827e8877ee339beb467f10844
SinghCtn <chetansingh2k1@gmail.com>
2024-08-20 19:05:29 +0530

    functions in linked list

--- 06_Linked_list/01_linked_list.cpp
+++ 06_Linked_list/01_linked_list.cpp
@@ -17,7 +17,15 @@ class LinkedList{
 
         void createFromArray(int A[], int i);
         void display();
-        void displayR();
+        // void displayR();
+        int count();
+        // int countR(int c);
+        int sum();
+        int max();
+        int min();
+        Node * search(int x);
+        Node * searchH(int key);
+        void insert(int index, int x);
 };
 
 void LinkedList::createFromArray(int A[], int n){
@@ -46,29 +54,155 @@ void LinkedList::display(){
         p = p->next;
     }
     cout<<"NULL"<<endl;
+    p = head;
 }
 
 // for displaying elements recursively
-void LinkedList::displayR(){
-    static Node *p = head;
-    if(p){
-        cout<<p->data<<" --> ";
+// void LinkedList::displayR(){
+//     static Node *p = head;
+//     if(p){
+//         cout<<p->data<<" --> ";
+//         p = p->next;
+//         displayR();
+//     }else{
+//         cout<<"NULL"<<endl;
+//         p = head;
+//     }
+    
+// }
+
+int LinkedList::count(){
+    Node *p = head;
+    int c = 0; 
+    while(p){
+        c++;
         p = p->next;
-        displayR();
-    }else{
-        cout<<"NULL"<<endl;
     }
+    return c;
+}
+
+// Recursive counting 
+// int LinkedList::countR(int c){
+//     static Node *p = head;
+//     if(p){
+        
+//         p = p->next;
+//         c = countR(c) + 1;
+//     }
+//     p = head;
+//     return c;
+// }
+
+int LinkedList::sum(){
+    int s = 0;
+    Node *p = head;
+    while(p){
+        s += p->data;
+        p = p->next;
+    }
+    return s;
+}
+
+int LinkedList::max(){
+    int max = INT8_MIN;
+    Node *p = head;
+
+    while(p){
+        if(max < p->data) max = p->data;
+        p = p->next;
+    }
+
+    return max;
+}
+
+int LinkedList::min(){
+    int min = INT8_MAX;
+    Node *p = head;
+
+    while(p){
+        if(min > p->data) min = p->data;
+        p = p->next;
+    }
+
+    return min;
+}
+
+Node * LinkedList::search(int x){
+    Node *p = head;
+    while(p){
+        if(p->data == x)return p;
+        p = p->next;
+    }
+
+    return p;
+}
+
+// leniar search and iplementing move to head
+Node * LinkedList::searchH(int key){
+    Node *p = head;
+    Node *q = nullptr;
+    while(p){
+        if(p->data == key){
+            if(p == head){
+                return p;
+            }
+            q->next = p->next;
+            p->next = head;
+            head = p;
+            return p;
+        }
+        q = p;
+        p = p->next;
+    }
+    return nullptr;
+}
+
+// inserting in a linked list
+void LinkedList::insert(int index, int x){
+    Node *temp = new Node;
+    temp->data = x;
+
+    if(index == 0){
+        temp->next = head;
+        head = temp;
+        return;
+    }
+
+    Node *p = head;
+    for(int i = 1; i < index; i++){
+        p = p->next;
+    }
+
+    temp->next = p->next;
+    p->next = temp;
 }
 
 int main(){
     LinkedList list;
-    int A[] = {3, 5, 7, 10, 15};
+    int A[] = {3, 5, 7, 10, 15, 17};
 
-    list.createFromArray(A, 5);
+    list.createFromArray(A, 6);
+
+    // cout<<list.countR(0)<<endl;
+    cout<<"count: "<<list.count()<<endl;
+
+    cout<<"Sum: "<<list.sum()<<endl;
+
+    cout<<"Max: "<<list.max()<<endl;
+
+    cout<<"Min: "<<list.min()<<endl;
+
+    cout<<"At: "<<list.search(3)<<endl;
+    // cout<<"At: "<<list.searchH(7)<<endl;
+
+    list.insert(0, 2);
+    list.insert(3, 2);
+
     list.display();
-
-    LinkedList l2;
-    l2.displayR();
+    
+    
+    // list.displayR();
+    // list.displayR();
 
     return 0;
 }
