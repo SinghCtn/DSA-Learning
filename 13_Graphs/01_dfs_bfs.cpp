d658bebda5c2d399550f5d900ccdf12887906252
SinghCtn <chetansingh2k1@gmail.com>
2024-09-08 08:35:38 +0530

    updated

--- 13_Graphs/01_dfs_bfs.cpp
+++ 13_Graphs/01_dfs_bfs.cpp
@@ -73,6 +73,19 @@ void BFS(int G[][7], int start, int n){
 
 }
 
+void DFS(int G[][7], int start, int n){
+    static int visited[7] = {0};
+    if(visited[start] == 0){
+        cout<<start<<" ";
+        visited[start] = 1;
+        for(int j = 1; j < n; j++){
+            if(G[start][j] == 1 && visited[j] == 0){
+                DFS(G, j, n);
+            }
+        }
+    }
+}
+
 
 
 int main(){
@@ -87,5 +100,6 @@ int main(){
     };
 
     BFS(G, 4, 7);
+    DFS(G, 1, 7);
     return 0;
 }
